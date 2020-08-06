e86ce6d544926f6996d68cac5f0adf39d81f5df8
BABAPEER <BABAPEER@LAPTOP-9SNAFCN3>
2020-08-06 19:45:26 +0530

    commit

---
+++ Actitime/src/test/java/com/actitme/timetrack/Demo.java
@@ -0,0 +1,16 @@
+package com.actitme.timetrack;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo 
+{
+	@Test
+	public void runTest()
+	{
+		Reporter.log("successfully build",true);
+		Reporter.log("successfully build",true);
+		Reporter.log("successfully build",true);
+	}
+
+}
